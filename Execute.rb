f090f96af23ed8a4378096395d21dc03187137f8
Rajagopalan-M <rajagopalanmadasami@gmail.com>
2020-02-28 15:41:16 +0530

    Epic Link is replaced by Epic Name and created the output sheet with the columns which are present in Referrence sheet.

--- Execute.rb
+++ Execute.rb
@@ -18,12 +18,14 @@ class Jira
             default_directory: File.expand_path("Downloads").gsub('/', '\\')
         }
     }
-    @b = Watir::Browser.new :chrome, options: {prefs: prefs}, args: ['user-data-dir=C:\RajagopalanM']
+    @b = Watir::Browser.new :chrome, options: {prefs: prefs}, args: ['user-data-dir=C:\ChromeProfile\ChromeProfile']
     @b.window.maximize
     @b.goto 'https://jira.sapiens.com/secure/Dashboard.jspa'
-    # @b.text_field(name: 'os_username').set 'rajagopalan.m'
-    # @b.text_field(name: 'os_password').set 'gopalan@456'
-    # @b.button(name: 'login').click
+    if @b.text_field(name: 'os_username').present?
+      @b.text_field(name: 'os_username').set 'rajagopalan.m'
+      @b.text_field(name: 'os_password').set '*****'
+      @b.button(name: 'login').click
+    end
     @b.element(link: 'Issues').click
     @b.element(link: 'Search for issues').click
     @b.element(link: 'Advanced').click unless @b.element(link: 'Basic').present?
@@ -50,10 +52,12 @@ class Jira
   def fetchAndWrite
     @inputSheet.each.with_index(1) do |(k, arrs), index|
       #Read the Epic Key and Epic Name to form the Hash : (Epic Key=>Epic Name)
-      @b.textarea(id: 'advanced-search').set "project = #{k.first[0..3]} AND issuetype = Epic", :return
+      puts k.first
+      @b.textarea(id: 'advanced-search').set "project = #{k.first[0..2]} AND issuetype = Epic", :return
       FileUtils.rm_rf Dir.glob(File.expand_path("Downloads/*"))
       @b.span(text: 'Export').click
       @b.link(id: 'currentCsvFields').click
+      @b.button(id: 'csv-export-dialog-export-button').click
       sleep 1
       @b.wait_while do
         @b.wait_until { Dir.glob(File.expand_path("Downloads/*")).count > 0 }
@@ -67,53 +71,78 @@ class Jira
       workBook = WriteExcel.new
       arrs.each do |arr|
         sheet = workBook.createSheet(arr[2])
-        @b.textarea(id: 'advanced-search').set arr[3], :return
-        @b.button(title: 'Columns').click
-        @reference[[arr[0], arr[2]]].each do |field|
-          @b.text_field(id: 'user-column-sparkler-input').set field[4]
-          @b.checkbox(xpath: "//ul[@class='aui-list-section aui-last']/li/label/em[normalize-space()='#{field[4]}' and not(preceding-sibling::span)]/preceding-sibling::input").set
-        end unless @reference[[arr[0], arr[2]]].nil?
-        @b.button(value: 'Done').click
-        $stdout.sync = true
-        unless @b.div(class: ["aui-message", "error"]).present?
-          sleep 0.5
-          @b.scroll.to :top
-          @b.span(text: 'Export').click
-          @b.link(id: 'currentCsvFields').click
-          sleep 2
-          filename = nil
-          @b.wait_while do
-            filename = Dir.glob(File.expand_path("Downloads\\*.*")).last
-            filename.include? 'crdownload'
-          end
-          table = CSV.read(filename)
-          table = @reference[[arr[0], arr[2]]].map { |row| table.transpose.select { |tableRow| tableRow[0].eql? row[2] }.pop }.reject { |x| x.to_s.empty? }.transpose unless @reference[[arr[0], arr[2]]].nil?
+        flag = false
+        # //span[@class='results-count-total results-count-link']
 
-          resultColumn = @reference[[arr[0], arr[2]]].map { |x| x[4] } unless @reference[[arr[0], arr[2]]].nil?
+        table = []
+        query = nil
+        loop do
 
-          #Form the reference hash(this is used for replacing the csv headers with our headers)
-          h = @referenceSheet.drop(1).each_with_object({}) do |value, h|
-            h[value[2]] = value[4]
+          if flag.eql? false
+            @b.textarea(id: 'advanced-search').set arr[3], :return
+          else
+            @b.textarea(id: 'advanced-search').set query, :return
           end
-          # Changing the table header with the our header.
-          table[0].each_with_index do |v, index|
-            table[0][index] = h[v]
-          end
-          #Replace the Epic Key with Epic Name
-          ind = nil
-          table.each_with_index do |row, index|
-            if index.eql? 0
-              ind = row.index("Epic Link")
-              # row[4] = 'Epic Name'
-              next
+
+          @b.button(title: 'Columns').click
+          @reference[[arr[0], arr[2]]].each do |field|
+            @b.text_field(id: 'user-column-sparkler-input').set field[4]
+            @b.checkbox(xpath: "//ul[@class='aui-list-section aui-last']/li/label/em[normalize-space()='#{field[4]}' and not(preceding-sibling::span)]/preceding-sibling::input").set
+          end unless @reference[[arr[0], arr[2]]].nil?
+          @b.button(value: 'Done').click
+          $stdout.sync = true
+          unless @b.div(class: ["aui-message", "error"]).present?
+            sleep 0.5
+            @b.scroll.to :top
+            @b.span(text: 'Export').click
+            @b.link(id: 'currentCsvFields').click
+            @b.button(id: 'csv-export-dialog-export-button').click
+            sleep 2
+            filename = nil
+            @b.wait_while do
+              filename = Dir.glob(File.expand_path("Downloads\\*.*")).last
+              filename.include? 'crdownload'
             end
-            row[ind] = issue_key[row[ind]]
+            csvTable = CSV.read(filename)
+            table << @reference[[arr[0], arr[2]]]
+                         .map { |row| csvTable.transpose.select { |tableRow| tableRow[0].eql? row[2] }.pop }
+                         .reject { |x| x.to_s.empty? }
+                         .transpose unless @reference[[arr[0], arr[2]]].nil?
+            countOfTotalRows = @b.span(class: ["results-count-total", "results-count-link"]).text.to_i
+            if countOfTotalRows < 1000
+              break
+            else
+              key = table[0][0].index('Issue key')
+              row = table.flatten(1).transpose[key]
+              query = arr[3].split(/(project\s+=\s+\w+)/).drop(1).join(" and Key > #{row.last} ")
+              flag = true
+            end
           end
-          table = table.transpose.select { |x| resultColumn.include? x[0].to_s.strip }.transpose
         end
+        table = table.flatten(1)
+        resultColumn = @reference[[arr[0], arr[2]]].map { |x| x[4] } unless @reference[[arr[0], arr[2]]].nil?
+
+        #Form the reference hash(this is used for replacing the csv headers with our headers)
+        h = @referenceSheet.drop(1).each_with_object({}) do |value, h|
+          h[value[2]] = value[4]
+        end
+        # Changing the table header with the our header.
+        table[0].each_with_index do |v, index|
+          table[0][index] = h[v]
+        end
+        #Replace the Epic Key with Epic Name
+        ind = nil
+        table.each_with_index do |row, index|
+          if index.eql? 0
+            ind = row.index("Epic Link")
+            # row[4] = 'Epic Name'
+            next
+          end
+          row[ind] = issue_key[row[ind]]
+        end
+        table = table.transpose.select { |x| resultColumn.include? x[0].to_s.strip }.transpose
         sheet.enterTheData(table, [table[0].index("Summary"), 50]) unless table.nil?
       end
-
       workBook.write(File.expand_path("Output/#{(k[0] + " " + k[1] + " " + "File" + " ").to_s + Date.today.strftime('%Y%m%d').gsub('-', '_')}.xlsx"))
     end
     FileUtils.rm_rf File.expand_path("Downloads")
